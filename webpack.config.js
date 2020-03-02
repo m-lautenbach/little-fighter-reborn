166b1ecdc14e585b9fc2bb73c7563251ac5ea755
Markus Lautenbach <markus.lautenbach@gmail.com>
2020-03-02 21:13:11 +0100

    Add bundle to html via webpack
    Good for html modifications that depend on webpack output, e.g. file hashes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+  ],
+}
