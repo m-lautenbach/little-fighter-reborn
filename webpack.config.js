572ff517ed5eba6b0412e1d300bcb018da8ff5b0
Markus Lautenbach <markus.lautenbach@gmail.com>
2020-03-18 20:20:58 +0100

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -30,6 +31,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CopyPlugin([
+      { from: './src/favicon.png' },
+    ]),
   ],
   devtool: 'inline-source-map',
   devServer: {
