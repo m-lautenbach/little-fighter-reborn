5371e192a82593b5520d374b8a428071fa0946bb
Markus Lautenbach <markus.lautenbach@gmail.com>
2020-03-20 22:37:29 +0100

    Remove compression
    No working with cloudflare and not needed.
    Everything already get's compressed by default and asset size is not acceptable.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,9 +9,6 @@ module.exports = merge(common, {
       analyzerMode: 'disabled',
       generateStatsFile: true,
       statsFilename: '../stats.json',
-    }),
-    new CompressionPlugin({
-      deleteOriginalAssets: true,
     }),
   ],
 })
