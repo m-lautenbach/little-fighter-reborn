f87b3f17405df898f1d9a47651385c13313f66c1
Markus Lautenbach <markus.lautenbach@gmail.com>
2020-03-21 21:22:35 +0100

    Scaling and load sprites

--- src/breakout-phaser.js
+++ src/breakout-phaser.js
@@ -1,13 +1,31 @@
-function preload() {
-}
-
-function create() {
-}
+import paddleSprite from './assets/images/paddle_128_32.png'
+import ballSprite from './assets/images/ball_32_32.png'
+import brick1Sprite from './assets/images/brick1_64_32.png'
+import brick2Sprite from './assets/images/brick2_64_32.png'
+import brick3Sprite from './assets/images/brick3_64_32.png'
 
 let game
+let ball
 
 export const start = async () => {
   const Phaser = await import(/* webpackChunkName: "phaser" */ 'phaser')
+
+  function preload() {
+    this.load.image('ball', ballSprite)
+    this.load.image('paddle', paddleSprite)
+    this.load.image('brick1', brick1Sprite)
+    this.load.image('brick2', brick2Sprite)
+    this.load.image('brick3', brick3Sprite)
+  }
+
+  function create() {
+    ball = this.add.sprite(50, 50, 'ball')
+  }
+
+  function update() {
+    ball.x += 1
+    ball.y += 1
+  }
 
   for (let existingCanvas of document.getElementsByTagName('canvas')) {
     existingCanvas.remove()
@@ -15,14 +33,19 @@ export const start = async () => {
 
   game = new Phaser.Game({
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 480,
+    height: 320,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    backgroundColor: '#eee',
+    scene: { preload, create, update },
     physics: {
       default: 'arcade',
-    },
-    scene: {
-      preload: preload,
-      create: create,
+      arcade: {
+        gravity: false,
+      },
     },
   })
 }
