affbc87d9dda308562276ea2d7ac6726f7b9730a
Markus Lautenbach <markus.lautenbach@actano.de>
2020-05-21 22:08:41 +0200

    Use walking speed from file

--- src/progression/updateDirection.js
+++ src/progression/updateDirection.js
@@ -1,6 +1,5 @@
 import inputState from '../inputState'
-
-const movementSpeed = 130
+import assetCache from '../assetCache'
 
 export default (actor) => {
   const { KeyW, KeyA, KeyS, KeyD, ArrowLeft, ArrowUp, ArrowRight, ArrowDown } = inputState
@@ -8,6 +7,9 @@ export default (actor) => {
   const right = KeyD || ArrowRight
   const up = KeyW || ArrowUp
   const down = KeyS || ArrowDown
+  const characterData = assetCache.data.characters[actor.character].bmp
+  // convert from pixel per TU (1/30s) to pixel per second
+  const movementSpeed = characterData.walking_speed * 30
   actor.direction = left && !right && 'left' || right && !left && 'right' || actor.direction
   actor.velocity.x = ((left && !right) ? -1 : (right && !left) ? 1 : 0) * movementSpeed
   actor.velocity.y = ((down && !up) ? -1 : (up && !down) ? 1 : 0) * movementSpeed
