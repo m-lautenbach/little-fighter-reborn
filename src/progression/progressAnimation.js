8698092967bdedf25a0a7eb510684b3c8d767e97
Markus Lautenbach <markus.lautenbach@actano.de>
2020-05-22 22:26:35 +0200

    Add missing wait time
    Wait time needs always 1 TU added

--- src/progression/progressAnimation.js
+++ src/progression/progressAnimation.js
@@ -3,8 +3,8 @@ import getFrameMap from '../getFrameMap'
 export default ({ character, animation }, newTimestamp) => {
   const { id, frame, start } = animation
   const { frames } = getFrameMap(character)[id]
-  // one TU (time unit) === 1/30s
-  const currentFrameEnded = (frames[frame].wait * (1000 / 30)) < (newTimestamp - start)
+  // one TU (time unit) === 1/30s; always +1
+  const currentFrameEnded = ((frames[frame].wait + 1) * (1000 / 30)) < (newTimestamp - start)
   const isLastFrame = frame === (frames.length - 1)
   const nextFrame = isLastFrame ? (frames[frame].next === 999 ? 0 : frame) : frame + 1
 
