494c6a10f004c5455229a4b5231bcd77997c61bf
Markus Lautenbach <markus.lautenbach@actano.de>
2020-05-22 22:31:26 +0200

    Use data file for depth walking speed as well

--- src/progression/updateDirection.js
+++ src/progression/updateDirection.js
@@ -10,7 +10,11 @@ export default (actor) => {
   const characterData = assetCache.data.characters[actor.character].bmp
   // convert from pixel per TU (1/30s) to pixel per second
   const movementSpeed = characterData.walking_speed * 30
+  // For some reason in data files, z is depth and y is up.
+  // Also, we need to double the speed, as we calculate perspective,
+  //     which halves the speed on screen for depth movement.
+  const yMovementSpeed = characterData.walking_speedz * 30 * 2
   actor.direction = left && !right && 'left' || right && !left && 'right' || actor.direction
   actor.velocity.x = ((left && !right) ? -1 : (right && !left) ? 1 : 0) * movementSpeed
-  actor.velocity.y = ((down && !up) ? -1 : (up && !down) ? 1 : 0) * movementSpeed
+  actor.velocity.y = ((down && !up) ? -1 : (up && !down) ? 1 : 0) * yMovementSpeed
 }
