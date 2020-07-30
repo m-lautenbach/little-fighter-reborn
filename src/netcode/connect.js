b71dbb156fb46186f6d710b74a4661636ebc0e6d
Markus Lautenbach <markus.lautenbach@actano.de>
2020-07-30 22:42:29 +0200

    Retry connection
    Heroku instance might be suspended

--- src/netcode/connect.js
+++ src/netcode/connect.js
@@ -1,12 +1,21 @@
 import io from 'socket.io-client'
 import { v4 as uuid } from 'uuid'
+import { operation } from 'retry'
+
 import registerHandlers from './registerHandlers'
 
 export default () => {
   const id = uuid()
-  const socket = io('https://gentle-mountain-79659.herokuapp.com')
+  const connectToSocketIoServer = operation()
+  connectToSocketIoServer.attempt(() => {
+    try {
+      let socket = io('https://gentle-mountain-79659.herokuapp.com')
 
-  socket.emit('register', { id })
-  console.debug(`I am ${id}`)
-  registerHandlers(socket)
+      socket.emit('register', { id })
+      console.debug(`I am ${id}`)
+      registerHandlers(socket)
+    } catch (ex) {
+      operation.retry(ex)
+    }
+  })
 }
